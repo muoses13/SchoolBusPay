209f3e25faf414582e4963db426debff707e9a95
muoses.muller <muoses.muller@gmail.com>
2025-10-20 18:26:08 +0000

    cinfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: ["https://3000-firebase-schoolbuspay-1760977530653.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev"],
+  },
 };
 
 export default nextConfig;
